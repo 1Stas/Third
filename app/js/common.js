e35e441dbb253001da80d38bbcc5bb32a529434c
Stas <afkfen7@gmail.com>
2019-06-02 12:50:43 +0300

    02 06 19

--- app/js/common.js
+++ app/js/common.js
@@ -18,6 +18,25 @@ $(document).ready(function() {
 	});
 });
 
+$(document).ready(function() {
+	$('.navbar_form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#text',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#text';
+				}
+			}
+		}
+	});
+});
 // Карусель.
 
 $(document).ready(function(){
